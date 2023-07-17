2b19617c45416e6615c8c0ea418cf031d813d337
Marton Gergo <gergo.marton@hotmail.com>
2023-07-17 21:23:35 +0200

    remove unused imports

--- example/src/main/java/com/coyoteforms/example/configuration/ValidatorProvider.java
+++ example/src/main/java/com/coyoteforms/example/configuration/ValidatorProvider.java
@@ -8,17 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class ValidatorProvider {
--- example/src/main/java/com/coyoteforms/example/controller/FormController.java
+++ example/src/main/java/com/coyoteforms/example/controller/FormController.java
@@ -1,18 +1,12 @@
 package com.coyoteforms.example.controller;
 
 import com.coyoteforms.example.dto.LocationDto;
-import com.coyoteforms.example.validation.ValidLocation;
 import com.coyoteforms.validator.CoyoteFormValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/forms/location-form")
